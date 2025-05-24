56bac396aa301115d64ccdd6760184d007d67742
sriram A <sriram.coder2004@gmail.com>
2025-05-24 16:33:42 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'./',
+  base: '/', // ✅ this is correct for custom domains
   plugins: [react()],
 })
